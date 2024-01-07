962711be0f8407e08e493a6922ac55e04f1cf32a
Artem Safin <artemsafin.webdev@gmail.com>
2024-01-07 12:41:57 +0300

    Фикс

--- app.js
+++ app.js
@@ -2,28 +2,29 @@ document.addEventListener("DOMContentLoaded", () => {
   let lenis = null;
 
   if (!window.matchMedia("(hover: hover)").matches) {
-    lenis = new Lenis({
-      smoothWheel: true,
-    });
-
-    lenis.on("scroll", ScrollTrigger.update);
-
-    gsap.ticker.add((time) => {
-      if (lenis) {
-        lenis.raf(time * 1000);
-      }
-    });
-
-    gsap.ticker.lagSmoothing(0);
   }
+
+  lenis = new Lenis({
+    smoothWheel: true,
+  });
+
+  lenis.on("scroll", ScrollTrigger.update);
+
+  gsap.ticker.add((time) => {
+    if (lenis) {
+      lenis.raf(time * 1000);
+    }
+  });
+
+  gsap.ticker.lagSmoothing(0);
 
   const sections = Array.from(document.querySelectorAll("section"));
 
   sections.forEach((section, index) => {
     if (index > sections.length - 2) return;
     ScrollTrigger.create({
-      start: "bottom bottom",
-      end: () => `bottom+=${window.innerHeight} bottom`,
+      start: "top top",
+      end: 999999999999,
       trigger: section,
       pin: true,
       pinSpacing: false,
